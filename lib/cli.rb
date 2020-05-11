7720ad491d57d10b2e8a193bd0f4a764186a1c17
Mauricio <maugravena@gmail.com>
2020-05-11 13:09:15 -0300

    improved organization and implementation of the methods that create tables

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,59 +1,6 @@
-require 'json'
-require 'terminal-table'
-
 class CLI
   def self.run
     State.all_states
     City.all_cities
   end
-end
-
-def hash_to_array_values(arr)
-  res_arr = []
-  arr.each do |i|
-    res_arr << i.values
-  end
-  res_arr
-end
-
-def rows(data)
-  result = data[0]['res']
-  hash_to_array_values(result)
-end
-
-def remove_strings(arr)
-  new_arr = []
-  arr.each do |i|
-    new_arr << i.delete_if { |obj| (obj.is_a? String) }
-  end
-  new_arr.flatten
-end
-
-def table(title, rows)
-  Terminal::Table.new :title => title, :headings => ['Nome', 'Frequencia', 'Ranking'], :rows => rows
-end
-
-def state_tables(id)
-  puts general_table = table('Geral', rows(ranking_by_state(id)))
-  puts female_table = table('Sexo feminino', rows(ranking_by_state_female(id)))
-  puts male_table = table('Sexo masculino', rows(ranking_by_state_male(id)))
-end
-
-def city_tables(id)
-  puts general_table = table('Geral', rows(ranking_by_city(id)))
-  puts female_table = table('Sexo feminino', rows(ranking_by_city_female(id)))
-  puts male_table = table('Sexo masculino', rows(ranking_by_city_male(id)))
-end
-
-def frequency_table(names)
-  period = ['1930', '1930-1940', '1940-1950', '1950-1960', '1960-1970', '1970-1980', '1980-1990', '1990-2000', '2000-2010']
-  title = 'Frequencia ao longo dos anos'
-  rows_name1 = remove_strings rows(name_frequency(names[0]))
-  if names.length > 1
-    rows_name2 = remove_strings rows(name_frequency(names[1]))
-    rows_names = period.zip(rows_name1, rows_name2)
-    return Terminal::Table.new :title => title, :headings => ['Periodo', names[0], names[1]], :rows => rows_names
-  end
-  rows = period.zip(rows_name1)
-  return Terminal::Table.new :title => title, :headings => ['Periodo', names[0]], :rows => rows
 end
--- lib/names.rb
+++ lib/names.rb
@@ -39,36 +39,24 @@ class Name
       '&sexo='
     end
 
-    def by_state(state_initials)
-      state = State.find_by(initials: state_initials.upcase)
-      return 'Estado não encontrado' if state.blank?
-
+    def by_state(state)
       state_id = state['id']
       url = "#{ranking_url}#{locality_filter}#{state_id}"
       get_data(url)
     end
 
-    def by_city(city_name)
-      city = City.where('lower(name) = ?', city_name.downcase)
-      return 'Cidade não encontrada' if city.blank?
-
+    def by_city(city)
       city_id = city[0]['id']
       get_data("#{ranking_url}#{locality_filter}#{city_id}")
     end
 
-    def by_state_and_gender(state_initials, gender)
-      state = State.find_by(initials: state_initials.upcase)
-      return 'Estado não encontrado' if state.blank?
-
+    def by_state_and_gender(state, gender)
       state_id = state['id']
       url = "#{ranking_url}#{locality_filter}#{state_id}#{gender_filter}#{gender}"
       get_data(url)
     end
 
-    def by_city_and_gender(city_name, gender)
-      city = City.where('lower(name) = ?', city_name.downcase)
-      return 'Cidade não encontrada' if city.blank?
-
+    def by_city_and_gender(city, gender)
       city_id = city[0]['id']
       url = "#{ranking_url}#{locality_filter}#{city_id}#{gender_filter}#{gender}"
       get_data(url)
@@ -76,6 +64,44 @@ class Name
 
     def name_frequency_for_decades(name)
       get_data("#{names_url}/#{name}")
+    end
+
+    # Primeira opção - ranking de nomes por estado
+    def state_tables(state_initials)
+      state = State.find_by(initials: state_initials.upcase)
+      return p 'Estado não encontrado' if state.blank?
+
+      puts table('Geral', rows(by_state(state)))
+      puts table('Sexo feminino', rows(by_state_and_gender(state, 'f')))
+      puts table('Sexo masculino', rows(by_state_and_gender(state, 'm')))
+    end
+
+    # Segunda opção - ranking de nomes por cidade
+    def city_tables(city_name)
+      city = City.where('lower(name) = ?', city_name.downcase)
+      return p 'Cidade não encontrada' if city.blank?
+
+      puts table('Geral', rows(by_city(city)))
+      puts table('Sexo feminino', rows(by_city_and_gender(city, 'f')))
+      puts table('Sexo masculino', rows(by_city_and_gender(city, 'm')))
+    end
+
+    # Terceira opção - ranking de um nome específico ao longo de décadas
+    # Recebe um array como parâmetro
+    def frequency_table(names)
+      period = ['1930', '1930-1940', '1940-1950', '1950-1960', '1960-1970', '1970-1980', '1980-1990', '1990-2000', '2000-2010']
+      title = 'Frequencia ao longo dos anos'
+      rows_name1 = remove_strings rows(name_frequency_for_decades(names[0]))
+
+      # Melhorar lógica, não usar um número mágico
+      if names.length == 2
+        rows_name2 = remove_strings rows(name_frequency_for_decades(names[1]))
+        rows_names = period.zip(rows_name1, rows_name2)
+        return puts Terminal::Table.new :title => title, :headings => ['Periodo', names[0], names[1]], :rows => rows_names
+      end
+
+      rows = period.zip(rows_name1)
+      return puts Terminal::Table.new :title => title, :headings => ['Periodo', names[0]], :rows => rows
     end
 
     private
@@ -86,6 +112,34 @@ class Name
       rescue RestClient::ExceptionWithResponse => e
         puts e.response
       end
+    end
+
+    def hash_to_array_values(arr)
+      res_arr = []
+      arr.each do |i|
+        res_arr << i.values
+      end
+      res_arr
+    end
+
+    # Monta as linhas para a tabela baseado no dados
+    def rows(data)
+      result = data[0]['res']
+      hash_to_array_values(result)
+    end
+
+    # Para limpar dados vindos de um nome específico
+    def remove_strings(arr)
+      new_arr = []
+      arr.each do |i|
+        new_arr << i.delete_if { |obj| (obj.is_a? String) }
+      end
+      new_arr.flatten
+    end
+
+    # Tabela para usar nas opções de estados e cidades
+    def table(title, rows)
+      Terminal::Table.new :title => title, :headings => ['Nome', 'Frequencia', 'Ranking'], :rows => rows
     end
   end
 end
